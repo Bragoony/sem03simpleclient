6b86ad71de123968063f342240b0a37a4173303f
Brage Kjemperud <bragebigrude@gmail.com>
2023-04-21 11:08:41 +0000

    perfekt

--- tcp_client.go
+++ tcp_client.go
@@ -28,7 +28,7 @@ func main() {
 		log.Fatal(err)
 	} 
 	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	//log.Printf("Kryptert melding: %s", response)
 	dekryptertMelding := mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+	log.Println("reply from proxy: ", string(dekryptertMelding))
 }
